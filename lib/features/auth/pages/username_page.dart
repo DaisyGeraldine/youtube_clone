9ce2f53fe4d98a83a7c9efaf353156756c6ed525
Tachi <105659277+DaisyGeraldine@users.noreply.github.com>
2025-03-18 10:05:06 -0500

    feat: :construction: username validation logic

--- lib/features/auth/pages/username_page.dart
+++ lib/features/auth/pages/username_page.dart
@@ -1,19 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone/cores/widgets/flat_button.dart';
+import 'package:youtube_clone/features/auth/repository/user_data_service.dart';
 
-class UsernamePage extends StatelessWidget {
-  const UsernamePage({super.key});
+final formKey = GlobalKey<FormState>();
+
+class UsernamePage extends ConsumerStatefulWidget {
+  final String displayName;
+  final String email;
+  final String profilePic;
+  const UsernamePage({
+    required this.displayName,
+    required this.email,
+    required this.profilePic
+  });
+
+  @override
+  ConsumerState<UsernamePage> createState() => _UsernamePageState();
+}
+
+class _UsernamePageState extends ConsumerState<UsernamePage> {
+  final TextEditingController _usernameController = TextEditingController();
+  bool isValidate = true;
+
+  void validateUsername() async {
+    final usersMap = await FirebaseFirestore.instance.collection('users').get();
+    final users = usersMap.docs.map((e) => e).toList();
+
+    String? targetUsername;
+
+    for (var user in users) {
+      if (_usernameController.text == user.data()['username']) {
+        targetUsername = user.data()['username'];
+        isValidate = false;
+        setState(() {});
+      }
+      if (_usernameController.text != targetUsername) {
+        isValidate = true;
+        setState(() {});
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(children: [
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 14, vertical: 20),
+          padding: EdgeInsets.symmetric(horizontal: 14, vertical: 26),
           child: Text('Enter the username',
               style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                  // fontSize: 20,
+                  // fontWeight: FontWeight.bold,
                   color: Colors.blueGrey)),
         ),
         Padding(
@@ -21,27 +62,63 @@ class UsernamePage extends StatelessWidget {
             left: 15,
             right: 15,
           ),
-          child: TextFormField(
-            decoration: InputDecoration(
-              suffix: Icon(
-                Icons.verified_user_rounded,
-              ),
-              suffixIconColor: Colors.green,
-              hintText: 'Username',
-              contentPadding: EdgeInsets.symmetric(horizontal: 10),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.blue),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.blue),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.green),
+          child: Form(
+            child: TextFormField(
+              onChanged: (value) {
+                validateUsername();
+              },
+              autovalidateMode: AutovalidateMode.always,
+              validator: (value) {
+                return isValidate ? null : "username already taken";
+              },
+              key: formKey,
+              controller: _usernameController,
+              decoration: InputDecoration(
+                suffix: isValidate
+                    ? Icon(
+                        Icons.verified_user_rounded,
+                      )
+                    : Icon(
+                        Icons.cancel,
+                      ),
+                suffixIconColor: isValidate ? Colors.green : Colors.red,
+                hintText: 'Insert Username',
+                contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.green),
+                ),
               ),
             ),
+          ),
+        ),
+        const Spacer(),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 30, left: 8, right: 8),
+          child: FlatButton(
+            text: 'CONTINUE',
+            onPressed: () async {
+              // add users data
+              isValidate
+                  ? await ref
+                      .read(userDataServiceProvider)
+                      .addUserDataToFirestore(
+                          displayName: widget.displayName,
+                          username: _usernameController.text,
+                          email: widget.email,
+                          photoURL: widget.profilePic,
+                          description: "")
+                  : null;
+            },
+            colour: isValidate ? Colors.green : Colors.green.shade200,
           ),
         ),
       ])),
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtube_clone/cores/screens/loader.dart';
 import 'package:youtube_clone/features/auth/pages/login_page.dart';
+import 'package:youtube_clone/features/auth/pages/username_page.dart';
 import 'package:youtube_clone/firebase_options.dart';
 import 'package:youtube_clone/home_page.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -28,28 +30,39 @@ class MyApp extends ConsumerWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return Scaffold(
-              body: Center(
-                child: Text('Something went wrong'),
-              ),
-            );
-          } else if (snapshot.hasData) {
-            return HomePage();
-          } else {
-            return LoginPage();
+          if (!snapshot.hasData) {
+            return const LoginPage();
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Loader();
           }
+
+          return StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("users")
+                .doc(FirebaseAuth.instance.currentUser!.uid)
+                .snapshots(),
+            builder: (context, snapshot) {
+              final user = FirebaseAuth.instance.currentUser;
+              if (!snapshot.hasData || !snapshot.data!.exists) {
+                return UsernamePage(
+                  displayName: user!.displayName!,
+                  email: user.email!,
+                  profilePic: user.photoURL!,
+                );
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return Loader();
+              }
+              return HomePage();
+            },
+          );
+
+          // return const UsernamePage(
+          //   displayName: ,
+          //   email: ,
+          //   profilePic: ,
+          // );
         },
       ),
     );
   }
 }
-
-
-
